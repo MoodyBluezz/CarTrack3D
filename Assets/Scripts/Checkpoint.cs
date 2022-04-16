ccc8176dcfa41a179ada3555a3f8f7bff433dfc2
MoodyBluezz <designerorprogrammer@gmail.com>
2022-04-16 22:15:36 +0600

    added fix for finishing only if player goes front

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -7,8 +7,11 @@ public class Checkpoint : MonoBehaviour
     {
         if (other.name.Equals("Car"))
         {
-            finishController.Count++;
-            GetComponent<Collider>().enabled = false;
+            Vector3 direction = other.transform.position - transform.position;
+            if (Vector3.Dot (transform.forward, direction) < 0) {
+                finishController.Count++;
+                GetComponent<Collider>().enabled = false;
+            }
         }
     }
 }
